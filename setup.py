888c5052cf2d68aeebb4dc3811b565b5dd7c42e7
Eduardo Blancas Reyes <github@blancas.io>
2022-06-24 18:35:06 +0200

    basic layout

---
+++ setup.py
@@ -0,0 +1,41 @@
+import re
+import ast
+from glob import glob
+from os.path import basename, splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('src/nbsnapshot/__init__.py', 'rb') as f:
+    VERSION = str(
+        ast.literal_eval(
+            _version_re.search(f.read().decode('utf-8')).group(1)))
+
+REQUIRES = []
+
+DEV = []
+
+setup(
+    name='nbsnapshot',
+    version=VERSION,
+    description=None,
+    license=None,
+    author=None,
+    author_email=None,
+    url=None,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    classifiers=[],
+    keywords=[],
+    install_requires=REQUIRES,
+    extras_require={
+        'dev': DEV,
+    },
+    entry_points={
+        # 'console_scripts': ['nbsnapshot=nbsnapshot.cli:cli'],
+    },
+)
